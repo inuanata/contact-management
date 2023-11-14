b909febf23c7a43a40a27e32414e74fee97250e0
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-14 16:16:33 +0700

    web response model

---
+++ src/main/java/contactmanagementrestfull/model/WebResponse.java
@@ -0,0 +1,15 @@
+package contactmanagementrestfull.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+  private T data;
+  private String errors;
+}
