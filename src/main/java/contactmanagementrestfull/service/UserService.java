40c73e7359c219f3ef5d0a2b593fe3c18f807204
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-18 18:39:34 +0700

    user service

---
+++ src/main/java/contactmanagementrestfull/service/UserService.java
@@ -0,0 +1,45 @@
+package contactmanagementrestfull.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import contactmanagementrestfull.entity.User;
+import contactmanagementrestfull.model.RegisterUserRequest;
+import contactmanagementrestfull.repository.UserRepository;
+import contactmanagementrestfull.security.BCrypt;
+import jakarta.validation.Validator;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private Validator validator;
+
+  @Transactional
+  public void register(RegisterUserRequest request) {
+    Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
+    if (constraintViolations.size() != 0) {
+      throw new ConstraintViolationException(constraintViolations);
+    }
+
+    if(userRepository.existsById(request.getUsername())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists");
+    }
+
+    User user = new User();
+    user.setUsername(request.getUsername());
+    user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt())); 
+    user.setName(request.getName());
+
+    userRepository.save(user);
+  }
+}
