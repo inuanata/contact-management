e64a00e5e6d391d30203f807b55b9cc82bd48c7c
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-18 18:39:04 +0700

    user repo

---
+++ src/main/java/contactmanagementrestfull/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package contactmanagementrestfull.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import contactmanagementrestfull.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+  Optional<User> findFirstByToken(String token);
+}
