168d10b54557126717fd820cfa1252cf833896e2
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-12-13 13:03:20 +0700

    feat: add get user ctrl

--- src/main/java/contactmanagementrestfull/controller/UserController.java
+++ src/main/java/contactmanagementrestfull/controller/UserController.java
@@ -3,11 +3,14 @@ package contactmanagementrestfull.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import contactmanagementrestfull.model.RegisterUserRequest;
 import contactmanagementrestfull.model.WebResponse;
+import contactmanagementrestfull.model.UserResponse;
+import contactmanagementrestfull.entity.User;
 import contactmanagementrestfull.service.UserService;
 
 @RestController
@@ -24,5 +27,14 @@ public class UserController {
   public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
     userService.register(request);
     return WebResponse.<String>builder().data("OK").build();
+  }
+
+  @GetMapping(
+    path = "/api/users/current",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public WebResponse<UserResponse> get(User user) {
+    UserResponse userResponse = userService.get(user);
+    return WebResponse.<UserResponse>builder().data(userResponse).build();
   }
 }
