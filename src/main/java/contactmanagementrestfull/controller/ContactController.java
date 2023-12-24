1c46b28d51165976a5b8e74bf8375191a2cef3ec
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-12-24 23:49:01 +0700

    feat: search contact ctrl

--- src/main/java/contactmanagementrestfull/controller/ContactController.java
+++ src/main/java/contactmanagementrestfull/controller/ContactController.java
@@ -59,32 +59,32 @@ public class ContactController {
         return WebResponse.<String>builder().data("OK").build();
     }
 
-    // @GetMapping(
-    //         path = "/api/contacts",
-    //         produces = MediaType.APPLICATION_JSON_VALUE
-    // )
-    // public WebResponse<List<ContactResponse>> search(User user,
-    //                                                  @RequestParam(value = "name", required = false) String name,
-    //                                                  @RequestParam(value = "email", required = false) String email,
-    //                                                  @RequestParam(value = "phone", required = false) String phone,
-    //                                                  @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-    //                                                  @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
-    //     SearchContactRequest request = SearchContactRequest.builder()
-    //             .page(page)
-    //             .size(size)
-    //             .name(name)
-    //             .email(email)
-    //             .phone(phone)
-    //             .build();
+    @GetMapping(
+            path = "/api/contacts",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<ContactResponse>> search(User user,
+                                                     @RequestParam(value = "name", required = false) String name,
+                                                     @RequestParam(value = "email", required = false) String email,
+                                                     @RequestParam(value = "phone", required = false) String phone,
+                                                     @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+        SearchContactRequest request = SearchContactRequest.builder()
+                .page(page)
+                .size(size)
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .build();
 
-    //     Page<ContactResponse> contactResponses = contactService.search(user, request);
-    //     return WebResponse.<List<ContactResponse>>builder()
-    //             .data(contactResponses.getContent())
-    //             .paging(PagingResponse.builder()
-    //                     .currentPage(contactResponses.getNumber())
-    //                     .totalPage(contactResponses.getTotalPages())
-    //                     .size(contactResponses.getSize())
-    //                     .build())
-    //             .build();
-    // }
+        Page<ContactResponse> contactResponses = contactService.search(user, request);
+        return WebResponse.<List<ContactResponse>>builder()
+                .data(contactResponses.getContent())
+                .paging(PagingResponse.builder()
+                        .currentPage(contactResponses.getNumber())
+                        .totalPage(contactResponses.getTotalPages())
+                        .size(contactResponses.getSize())
+                        .build())
+                .build();
+    }
 }
